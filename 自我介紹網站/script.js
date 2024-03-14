d407719eea9b5563a03300964de481df71d42c02
hyderson <80370102+hyderson@users.noreply.github.com>
2024-03-14 16:37:14 +0800

    Add files via upload

---
+++ 自我介紹網站/script.js
@@ -0,0 +1,15 @@
+ /*const skillHtml=document.getElementById('skill_html');
+skillHtml.addEventListener("mousedown",()=>{
+    skillHtml.classList.toggle('codeinglanhuages_content_2');
+})
+*/
+/*
+const Exp_cont1=document.getElementById('exp_cont1');
+function changeW(){
+    Exp_cont1.document.h1.innerHTML("Hi");
+    console.log("its working");
+}
+
+    Exp_cont1.addEventListener("mouseclick",changeW);
+*/
+   
